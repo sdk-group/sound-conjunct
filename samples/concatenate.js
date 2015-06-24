2f1bf4875eb3150f6bd4e597115adf06b0260185
YuuKohaku <heimaolee@gmail.com>
2015-06-24 12:24:25 +0600

    Update

--- samples/concatenate.js
+++ samples/concatenate.js
@@ -1,5 +1,5 @@
 var path = require("path")
-var concat = require("sound-conjunct").concatenate;
+var concat = require("../index").concatenate;
 
 var fnames = ["explosion-01.wav",
               "Media-Convert_test1_Alaw_Mono_VBR_8SS_16000Hz.wav",
@@ -8,7 +8,24 @@ var fnames = ["explosion-01.wav",
               "Media-Convert_test4_Ulaw_Mono_VBR_8SS_22050Hz.wav"];
 
 for (var e in fnames) {
-    fnames[e] = path.resolve(__dirname, "sound/" + fnames[0]);
+    fnames[e] = path.resolve(__dirname, "sounds", fnames[1]);
 }
 
-concat(fnames, path.resolve(__dirname, "out.wav"));
+//    var opts = {
+//        bits: assign.bits || 8,
+//        rate: assign.rate || 16000,
+//        channels: assign.channels || 1
+//    };
+
+var opts = {
+    bits: 16,
+    rate: 24000
+};
+
+concat(fnames, path.resolve(__dirname, "out.wav"), opts)
+    .then(function (res) {
+        console.log("RES:", res)
+    })
+    .catch(function (res) {
+        console.log("ERR:", res)
+    });
